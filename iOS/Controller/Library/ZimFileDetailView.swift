8c2686bc59f978fa5249aed78b7c3792b510095d
Chris Li <chris@kiwix.org>
2020-07-13 21:08:40 -0400

    list structure

--- iOS/Controller/Library/ZimFileDetailView.swift
+++ iOS/Controller/Library/ZimFileDetailView.swift
@@ -14,17 +14,48 @@ import RealmSwift
 
 @available(iOS 13.0, *)
 struct ZimFileDetailView: View {
-    let zimFile: ZimFile
     @ObservedObject private var viewModel: ViewModel
     
     init(zimFile: ZimFile) {
-        self.zimFile = zimFile
         self.viewModel = ViewModel(zimFile)
     }
     
     var body: some View {
         List {
-            if let language = viewModel.language { TitleDetailListRow(title: "Language", detail:  language)}
+            Section {
+                TitleDetailListRow(title: "Language", detail:  viewModel.language)
+                TitleDetailListRow(title: "Size", detail:  viewModel.size)
+                TitleDetailListRow(title: "Date", detail:  viewModel.creationDate)
+            }
+            Section {
+                TitleDetailListRow(title: "Index", detail:  viewModel.hasIndex)
+                TitleDetailListRow(title: "Pictures", detail:  viewModel.hasPictures)
+                TitleDetailListRow(title: "Videos", detail:  viewModel.hasVideos)
+                TitleDetailListRow(title: "Details", detail:  viewModel.hasDetails)
+            }
+            Section {
+                TitleDetailListRow(title: "Article Count", detail:  viewModel.articleCount)
+                TitleDetailListRow(title: "Media Count", detail:  viewModel.mediaCount)
+            }
+            Section {
+                TitleDetailListRow(title: "Creator", detail:  viewModel.creator)
+                TitleDetailListRow(title: "Publisher", detail:  viewModel.publisher)
+            }
+            Section {
+                TitleDetailListRow(title: "ID", detail:  viewModel.shortID)
+            }
+            Section {
+                Button(action: {
+                    
+                }) {
+                    HStack {
+                        Spacer()
+                        Text("Delete File").fontWeight(.medium)
+                        Spacer()
+                    }
+                }
+                .foregroundColor(.red)
+            }
         }
         .listStyle(GroupedListStyle())
         .environment(\.horizontalSizeClass, .regular)
@@ -35,9 +66,59 @@ struct ZimFileDetailView: View {
 fileprivate class ViewModel: ObservableObject {
     private let database = try? Realm(configuration: Realm.defaultConfig)
     let zimFile: ZimFile
+    private let unknown = "Unknown"
+    private let countFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        return formatter
+    }()
     
-    var language: String? {
-        Locale.current.localizedString(forLanguageCode: zimFile.languageCode)
+    var language: String {
+        Locale.current.localizedString(forLanguageCode: zimFile.languageCode) ?? unknown
+    }
+    
+    var size: String {
+        zimFile.sizeDescription ?? unknown
+    }
+    
+    var creationDate: String {
+        zimFile.creationDateDescription ?? unknown
+    }
+    
+    var hasIndex: String {
+        zimFile.hasIndex ? "Yes" : "No"
+    }
+    
+    var hasPictures: String {
+        zimFile.hasPictures ? "Yes" : "No"
+    }
+    
+    var hasVideos: String {
+        zimFile.hasVideos ? "Yes" : "No"
+    }
+    
+    var hasDetails: String {
+        zimFile.hasDetails ? "Yes" : "No"
+    }
+    
+    var articleCount: String {
+        countFormatter.string(for: zimFile.articleCount.value) ?? unknown
+    }
+    
+    var mediaCount: String {
+        countFormatter.string(for: zimFile.mediaCount.value) ?? unknown
+    }
+    
+    var creator: String {
+        zimFile.creator ?? unknown
+    }
+    
+    var publisher: String {
+        zimFile.publisher ?? unknown
+    }
+    
+    var shortID: String {
+        String(zimFile.id.prefix(8))
     }
     
     init(_ zimFile: ZimFile) {
@@ -49,7 +130,18 @@ fileprivate class ViewModel: ObservableObject {
 struct ZimFileDetailView_Previews: PreviewProvider {
     static var previews: some View {
         let zimFile = ZimFile(value: [
-            "languageCode": "en"
+            "id": "d87a3cf1-545b-49a0-ad51-108547970796",
+            "languageCode": "en",
+            "size": 1000000000,
+            "creationDate": Date(),
+            "hasIndex": true,
+            "hasPictures": false,
+            "hasVideos": true,
+            "hasDetails": false,
+            "articleCount": 10000000,
+            "mediaCount": 1000,
+            "creator": "Home Lab",
+            "publisher": "Kiwix"
         ])
         return ZimFileDetailView(zimFile: zimFile)
     }
